f50ea30312939cd4d987cdad0a41ca2dc1147b7a
sahajsatani <sahajsatani2005@gmail.com>
2024-04-19 11:54:09 +0530

    Add Array

---
+++ first.js
@@ -0,0 +1,32 @@
+// console.log("sahaj");   
+// let accnumber = 120
+// const nam = "sahaj"
+// var des = "surat"
+// let city
+// console.table([accnumber,nam,des,city])
+  
+// const game = "super";
+// // console.log(game.__proto__);
+// console.log(game.slice(-1,10));
+
+// const temp = "hi how are you!"
+// console.log(temp.replace("how",true));
+// let num = 123.45;
+// console.log(num.toFixed(1));
+// console.log(num.toPrecision(4));
+// let temp = 897656;
+// console.log(temp.toLocaleString('en-IN'));
+
+// let temp = Math.random();
+// let min=10;
+// let max=30;
+// console.log(Math.floor(temp * (max-min+1))+min);
+
+
+// let arr = [10,20,30,[1,2,3],90,[11,12,13,[101,102]]];
+// let temp = arr.flat();
+// console.log(temp);
+
+// console.log(Array.isArray("ok"));
+// let temp = Array.from("sahaj");
+// console.log(temp);
